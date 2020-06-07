3f13acee81f844c137ba3959d1026b9d3b472845
Elizabeth Samatepo <elizabeth.samatepo@tafensw.edu.au>
2020-06-07 16:38:17 +1000

    Updated to switch statements

--- StoreLocator.js
+++ StoreLocator.js
@@ -17,47 +17,75 @@ function askQuestion(question) {
 
 async function Program() {
 	console.log("Hello, Welcome to Liz's Storefinder!");
+
 	while (true) {
+	
 		console.log("[1] Sydney");
 		console.log("[2] Melbourne");
 		console.log("[3] Perth");
 		console.log("[4] None of the Above");
-		let userInput = await askQuestion("Please select your location from the options listed above: ");
-		if (userInput == "1") {
-			console.log("You have selected our Sydney Store, Please visit us at the address below:");
-			let sydneyLocation = {
-				Building: "Blacktown Mall",
-				Shop: 3,
-				Manager: "Kelsi"
-			};
-			console.log(sydneyLocation);
-		} else if (userInput == "2") {
-			console.log("You have selected our Melbourne Store, Please visit us at the address below:");
-			let melbourneLocation = {
-				Building: "Chadstone Mall",
-				shop: 442,
-				Manager: "Veronica"
-			};
-			console.log(melbourneLocation);
-		} else if (userInput == "3") {
-			console.log("You have selected our Perth Store, Please visit us at the address below:");
-			let perthLocation = {
-				Building: "Wahmoy Mall",
-				shop: 62,
-				Manager: "Monica"
-			};
-			console.log(perthLocation);
-		} else if (userInput == "4") {
-			console.log("Unfortunately, we are not in your location yet, please shop online!");
-			break;
-		} else {
-			console.log("Error: please enter a number 1 - 4");
-		}
-		console.log("");
-	}
-	console.log("Thank you for you for visiting Liz's site!");
-}
 
+		
+		let userInput = await askQuestion("Please select your location from the options listed above: ");
+		  switch (userInput){
+			 case "1" :
+			console.log("You have selected our Sydney Store. Please visit us as the addres below:")
+
+			let sydneyLocation = {
+				address : "14 Wahroonga Rd, North Sydney",
+				manager : "Kelse Mann",
+				phone: "042456499"
+			};
+			
+			console.log (sydneyLocation);
+		
+			break;
+		
+
+			case "2" : 
+
+			console.log("You have selected our Melbourne Store. Please visit us as the address below:");
+		
+			let melbourneLocation = {
+				Address: "223 Chadwick Road",
+				Manager: "Veronica",
+				phone: "045993405"
+			};
+
+			console.log(melbourneLocation)
+	
+			break;
+
+		  case "3": 
+	
+			console.log("You have selected our Perth Store. Please visit us at the address below:")
+			
+			 let perthLocation = {
+			   Address : "345 Wahmoy Lane",
+				Manager: "Monica Ndlovu",
+				phone : "039400024"
+			};
+              console.log(perthLocation);
+			break;
+			
+		 case "4" : 
+			console.log("Unfortunately, we are not in your location yet, please express your interest in having one of our stores in your city");
+			break;
+
+		case "5":
+
+		console.log("Thank you for you for visiting Liz's site!")
+
+		break;
+
+		 default: 
+			console.log("Error: please enter a number 1 - 4");
+
+
+		}
+
+	}
+}
 
 Program().then(() => {
 	process.exit(0);
